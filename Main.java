0a06d7f0edeb0ff23740dff34faae55fb89831bd
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-03-10 16:11:17 +0530

    added:compaision logic

--- Main.java
+++ Main.java
@@ -4,19 +4,27 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        double length1, length2;
 //line 1 variable
         int l1_x1 = 5;
         int l1_x2 = 6;
         int l1_y1 = 9;
         int l1_y2 = 111;
-        double length1 = Math.sqrt((l1_x2 - l1_x1)^2 + (l1_y2 - l1_y1)^2);
+        length1 = Math.sqrt((l1_x2 - l1_x1)^2 + (l1_y2 - l1_y1)^2);
 // line 2 variables
         int l2_x1 = 2;
         int l2_x2 = 8;
         int l2_y1 = 20;
         int l2_y2 = 21;
-        double length2 = Math.sqrt((l2_x2 - l2_x1)^2 + (l2_y2 - l2_y1)^2);
+        length2 = Math.sqrt((l2_x2 - l2_x1)^2 + (l2_y2 - l2_y1)^2);
         System.out.println("line1 length = " + length1);
         System.out.println("line2 length = " + length2);
+// comparing two lines
+        if (length1 == length2){
+            System.out.println("both lines are equal");
+        }
+        else {
+            System.out.println("both line are not equal");
+        }
     }
 }
