623c21f6d16526d0d8930f8b8b228c8e4d0abb68
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-03-10 15:16:52 +0530

    added:welcome tag

---
+++ Main.java
@@ -0,0 +1,9 @@
+// welcome to line comparison and computation program
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
